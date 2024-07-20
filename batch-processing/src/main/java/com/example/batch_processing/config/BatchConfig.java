85efc6fad879381504fcfc74f7151910b3a2aa94
Jonatan <jonatanav255@hotmail.com>
2024-07-19 22:55:26 -0600

    adding batch config

---
+++ batch-processing/src/main/java/com/example/batch_processing/config/BatchConfig.java
@@ -0,0 +1,10 @@
+package com.example.batch_processing.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+                // This class will be used to configure the batch job and steps
+}
