ea29e8d3bad0bc15816d7729f8665997c270b65d
Jonatan <jonatanav255@hotmail.com>
2024-07-22 21:35:06 -0600

    just some comments

--- batch-processing/src/main/java/com/example/batch_processing/config/JobCompletionNotificationListener.java
+++ batch-processing/src/main/java/com/example/batch_processing/config/JobCompletionNotificationListener.java
@@ -4,19 +4,16 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.stereotype.Component;
 
-// Indicates that this class is a Spring-managed component
-@Component
+@Component // Marks this as a Spring-managed component
 public class JobCompletionNotificationListener implements JobExecutionListener {
 
-    // This method is called before the job starts
     @Override
     public void beforeJob(JobExecution jobExecution) {
-        System.out.println("Job started");
+        System.out.println("Job started"); // Logs a message before the job starts
     }
 
-    // This method is called after the job finishes
     @Override
     public void afterJob(JobExecution jobExecution) {
-        System.out.println("Job finished");
+        System.out.println("Job finished"); // Logs a message after the job finishes
     }
 }
