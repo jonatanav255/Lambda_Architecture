487235ceb88f073e74bdadd3130240b2c260957f
Jonatan <jonatanav255@hotmail.com>
2024-07-22 21:34:51 -0600

    Adding JobParameters params

--- batch-processing/src/main/java/com/example/batch_processing/BatchProcessingApplication.java
+++ batch-processing/src/main/java/com/example/batch_processing/BatchProcessingApplication.java
@@ -2,33 +2,31 @@ package com.example.batch_processing;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Indicates that this is a Spring Boot application
-@SpringBootApplication
+@SpringBootApplication // Marks this as a Spring Boot application
 public class BatchProcessingApplication implements CommandLineRunner {
 
-    // Autowires the JobLauncher to launch the job
-    @Autowired
+    @Autowired // Injects the JobLauncher bean
     private JobLauncher jobLauncher;
 
-    // Autowires the job to be executed
-    @Autowired
+    @Autowired // Injects the Job bean
     private Job job;
 
-    // The main method to run the Spring Boot application
     public static void main(String[] args) {
-        SpringApplication.run(BatchProcessingApplication.class, args);
+        SpringApplication.run(BatchProcessingApplication.class, args); // Starts the Spring Boot application
     }
 
-    // This method is executed after the application context is loaded
     @Override
     public void run(String... args) throws Exception {
-        // Launches the job with empty parameters
-        jobLauncher.run(job, new JobParameters());
+        JobParameters params = new JobParametersBuilder()
+                .addLong("time", System.currentTimeMillis()) // Adds a unique parameter to the job to ensure it runs each time
+                .toJobParameters();
+        jobLauncher.run(job, params); // Launches the job with the specified parameters
     }
 }
